4e12f4a36259055b2d37372ecb4506b7aeded150
mseptiawan <mseptiawan017@gmail.com>
2025-12-19 23:59:51 +0700

    int

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProjectDetail } from "./pages/ProjectDetail";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
   return (
