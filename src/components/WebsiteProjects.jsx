cd08363cac71698f5fb2c578aca3008c1ba0660e
mseptiawan <mseptiawan017@gmail.com>
2025-12-20 09:54:35 +0700

    ada

--- src/components/WebsiteProjects.jsx
+++ src/components/WebsiteProjects.jsx
@@ -19,7 +19,7 @@ export const WebsiteProjects = () => {
         {webProjects.map((project, index) => (
           <Link
             key={index}
-            // Ubah dari /pro
+            // Ubah dari /
             to={`/project/${project.slug}`}
             className="group flex items-baseline gap-6 py-1"
           >
